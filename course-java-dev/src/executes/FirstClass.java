41cf79a86546650b73acc2d5430f68f2615b27cc
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-08-30 16:09:20 -0300

    Doing operations between whole numbers

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -2,24 +2,16 @@ package executes;
 
 public class FirstClass {
 	
-	/*Global variable is accessible for all*/
-	static int globalOlderAge = 30;
 	
 	/*Main is a method auto execute in Java*/
 	public static void main(String[] args) {
 		
-		/*Local variable because belongs only to this method*/
-		int olderAge = 18;
+		int grade1 = 90;
+		int grade2 = 70;
+		int grade3 = 50;
+		int grade4 = 88;
 		
-		method2();
-		
-		System.out.println("Value of local variable is = "+olderAge);
-
-	}
-	
-	public static void method2() {
-		 globalOlderAge ++;
-		 System.out.println("Value of global variable is =  "+globalOlderAge);
+		System.out.println("Grade average is = "+(grade1 + grade2 + grade3 + grade4)/4);
 	}
 
 }
