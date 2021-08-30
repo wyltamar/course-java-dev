b77f221e67748f16433409e2d7f0f7233fa38bf0
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-08-30 15:18:28 -0300

    Repository creation

---
+++ course-java-dev/src/executes/FirstClass.java
@@ -0,0 +1,23 @@
+package executes;
+
+public class FirstClass {
+	public static void main(String[] args) {
+		
+		int olderAge = 18;
+		
+		int oldAge = 60;
+		
+		int youngerAge = 16;
+		
+		int kidAge = 7; 
+		
+		String cpf = "123.456.789-54";
+		
+		System.out.println("Older age is = "+olderAge);
+		System.out.println("Old age is = "+oldAge);
+		System.out.println("Younger age is = "+youngerAge);
+		System.out.println("Kid age is = "+kidAge);
+		
+	}
+
+}
