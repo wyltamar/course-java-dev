11562a436071ae8ab93f200d2eca604ddf26ff8d
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-01 16:58:28 -0300

    Simple ternary operator

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -8,20 +8,24 @@ public class FirstClass {
 		double grade1 = 10;
 		double grade2 = 80;
 		double grade3 = 10;
-		double grade4 = 10;
+		double grade4 = 100;
 		double average = 0;
-		
-		average = (grade1 + grade2 + grade3 + grade4)/ 4;
-		
-		if(average >= 70) {
-			System.out.println("Approved: "+average);
-		}
-		else if(average >= 40 && average <= 69) {
-			System.out.println("Estudent recovering: "+average);
-		}
-		else {
-			System.out.println("Disapproved: "+average);
-		}
+
+		average = (grade1 + grade2 + grade3 + grade4) / 4;
+
+		/* Logical condition with IF and ELSE */
+		/*
+		 * if(average >= 70) { System.out.println("Approved: "+average); } else
+		 * if(average >= 40 && average <= 69) {
+		 * System.out.println("Student recovering: "+average); } else {
+		 * System.out.println("Disapproved: "+average); }
+		 */
+
+		/* Ternary operators */
+
+		String outputResultStudent = average >= 70 ? "Approved student" : (average >= 40 && average <= 69) ? "Recovering student" : "Disapproved Student" ;
+		System.out.println(outputResultStudent);
+
 	}
 
 }
