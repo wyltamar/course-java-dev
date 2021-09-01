2ac43f84ddc75056c2c5ca6feff70d43465d8f22
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-01 17:29:47 -0300

    Logical conditions nested

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -5,27 +5,25 @@ public class FirstClass {
 	/* Main is a method auto execute in Java */
 	public static void main(String[] args) {
 
-		double grade1 = 10;
+		double grade1 = 100;
 		double grade2 = 80;
-		double grade3 = 10;
+		double grade3 = 100;
 		double grade4 = 100;
 		double average = 0;
 
 		average = (grade1 + grade2 + grade3 + grade4) / 4;
 
-		/* Logical condition with IF and ELSE */
-		/*
-		 * if(average >= 70) { System.out.println("Approved: "+average); } else
-		 * if(average >= 40 && average <= 69) {
-		 * System.out.println("Student recovering: "+average); } else {
-		 * System.out.println("Disapproved: "+average); }
-		 */
-
-		/* Ternary operators */
-
-		String outputResultStudent = average >= 70 ? "Approved student" : (average >= 40 && average <= 69) ? "Recovering student" : "Disapproved Student" ;
-		System.out.println(outputResultStudent);
-
+		/*Logical operators nested*/
+		
+		if(average >= 50) {
+			if(average >= 70) {
+				System.out.println("Direct approved student");
+			}else {
+				System.out.println("Student is recovering");
+			}
+		}else {
+			System.out.println("Direct repproved student");
+		}
 	}
 
 }
