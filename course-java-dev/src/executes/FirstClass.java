8d8a3e622dcbce8a1f222bd110b8e63bd4866f51
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-02 10:46:16 -0300

    Relational operators

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	/* Main is a method auto execute in Java */
 	public static void main(String[] args) {
 		
-		/*
+		
 		double grade1 = 100;
 		double grade2 = 80;
 		double grade3 = 100;
@@ -16,7 +16,7 @@ public class FirstClass {
 
 		/*Logical operators nested*/
 		
-		/*if(average >= 50) {
+		if(average >= 50) {
 			if(average >= 70) {
 				System.out.println("Direct approved student");
 			}else {
@@ -24,46 +24,8 @@ public class FirstClass {
 			}
 		}else {
 			System.out.println("Direct repproved student");
-		} */
-		
-		/*SWICH CASE*/
-		
-		int day = 7;
-		
-		switch (day) {
-		case 1:
-			System.out.println("Sunday");
-			break;
-			
-		case 2:
-			System.out.println("Monday");
-			break;
-
-		case 3:
-			System.out.println("Tuesday");
-			break;
-		
-		case 4:
-			System.out.println("Wednesday");
-			break;
-			
-		case 5:
-			System.out.println("Thursday");
-			break;
-
-		case 6:
-			System.out.println("Friday");
-			break;
-
-		case 7:
-			System.out.println("Saturday");
-			break;
-	
-		default:
-			System.out.println("Invalid day");
-			break;
-		}
-		
+		} 
+				
 	}
 
 }
