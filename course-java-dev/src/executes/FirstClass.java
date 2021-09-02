0c313a3a25a6f12e3c3d9384382b7dc32b0ce03d
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-02 17:20:32 -0300

    Repeating structure WHILE and DO WHILE

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -5,27 +5,20 @@ public class FirstClass {
 	/* Main is a method auto execute in Java */
 	public static void main(String[] args) {
 		
+		int number = 0, number2 = 0;
 		
-		double grade1 = 100;
-		double grade2 = 80;
-		double grade3 = 100;
-		double grade4 = 100;
-		double average = 0;
-
-		average = (grade1 + grade2 + grade3 + grade4) / 4;
-
-		/*Logical operators nested*/
 		
-		if(average >= 50) {
-			if(average >= 70) {
-				System.out.println("Direct approved student");
-			}else {
-				System.out.println("Student is recovering");
-			}
-		}else {
-			System.out.println("Direct repproved student");
-		} 
-				
+		/*Check then run*/
+		while (number <= 10) {
+			System.out.println("The present number is: "+number);
+			number++;
+		}
+		
+		do {
+			System.out.println("The present number is: "+number2);
+			number2++;
+		} while (number2 <= 5);
+		
 	}
 
 }
