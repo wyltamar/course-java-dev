cdf2211a735da4cb624f225ad646dce036fa6dfd
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-05 20:50:14 -0300

    Repeating structure FOR

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -5,19 +5,15 @@ public class FirstClass {
 	/* Main is a method auto execute in Java */
 	public static void main(String[] args) {
 		
-		int number = 0, number2 = 0;
+		/*Repeating structure FOR*/ 
 		
-		
-		/*Check then run*/
-		while (number <= 10) {
+		for(int number = 0; number <= 10; number++) {
 			System.out.println("The present number is: "+number);
-			number++;
 		}
 		
-		do {
-			System.out.println("The present number is: "+number2);
-			number2++;
-		} while (number2 <= 5);
+		for(int number = 10; number >= 0; number--) {
+			System.out.println("The present number is: "+number);
+		}
 		
 	}
 
