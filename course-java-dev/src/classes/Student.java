f30fb7406ab448d22333acf4b7da8df3378b50a7
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-06 15:47:08 -0300

    Add attributes in class Student

--- course-java-dev/src/classes/Student.java
+++ course-java-dev/src/classes/Student.java
@@ -2,6 +2,17 @@ package classes;
 
 public class Student {
 	
+	String name;
+	String age;
+	String birthDate;
+	String generalRecord;
+	String cpfNumber;
+	String matherName;
+	String fatherName;
+	String enrollmentDate;
+	String schoolName;
+	String seriesEnrollment;
+	
 	
 
 }
