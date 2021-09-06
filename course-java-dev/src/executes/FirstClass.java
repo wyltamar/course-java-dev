2e5ea88e2d59155a200aeaeaa806e416238c0d08
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-06 09:41:08 -0300

    Creating a confirmation option

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -7,7 +7,7 @@ public class FirstClass {
 	/* Main is a method auto execute in Java */
 	public static void main(String[] args) {
 		
-		/*Data input with class JoptionPane */ 
+		/*Creating a confirmation option */ 
 		
 		int cars = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of cars: "));
 		int persons = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of persons: "));
@@ -15,7 +15,23 @@ public class FirstClass {
 		int division = cars / persons;
 		int rest = cars % persons;
 		
-		JOptionPane.showMessageDialog(null, "The division is "+division+ " car for each person and " +rest + " car(s) left.");
+		int answer = JOptionPane.showConfirmDialog(null, "Do you want to see the result of the split?");
+		
+		if(answer == 0) {
+			
+			JOptionPane.showMessageDialog(null, "The division result is: "+division);
+		}else {
+			JOptionPane.showMessageDialog(null, "User don't have want seen the result of the division!");
+		}
+		
+		answer = JOptionPane.showConfirmDialog(null, "Do you want to see the rest of the split?");
+		
+		if(answer == 0) {
+			JOptionPane.showMessageDialog(null, "The rest of the division is: "+rest);
+		}else {
+			JOptionPane.showMessageDialog(null, "User don't have want seen the rest of the division!");
+		}
+		
 
 	}
 
