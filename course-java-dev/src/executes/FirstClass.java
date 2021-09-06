66162ee6d04f50e78d33ef31cf601147335c1cf7
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-06 08:41:30 -0300

    Divisions and remains with MOD in Java

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -5,17 +5,13 @@ public class FirstClass {
 	/* Main is a method auto execute in Java */
 	public static void main(String[] args) {
 		
-		/*Repeating structure FOR with break */ 
+		/*Divisions and remains with MOD in Java*/ 
 		
-		for(int number = 0; number <= 10; number++) {
-			if(number == 3 || number == 6 || number == 9) {
-				
-				System.out.println("I found the number: "+number);
-				System.out.println("I'm runing...");
-				continue;
-			}
-			System.out.println("Looking for number...");
-		}
+		double car = 9;
+		double person = 2;
+		double rest = car % person;
+		
+		System.out.println("The rest is: "+rest+ " car(s)");
 
 	}
 
