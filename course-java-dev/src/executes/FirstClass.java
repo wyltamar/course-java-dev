f4c8cc21729e7b8955e8471e25bfd46388769859
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-09-27 10:46:50 -0300

    Removing repeat subjects

--- course-java-dev/src/executes/FirstClass.java
+++ course-java-dev/src/executes/FirstClass.java
@@ -15,35 +15,36 @@ public class FirstClass {
 		String name = JOptionPane.showInputDialog("Enter name:");
 		student4.setName(name);
 
-		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter age:"));
-		student4.setAge(age);
-
-		
-		  String birthDate = JOptionPane.showInputDialog("Enter birth date:");
-		  student4.setBirthDate(birthDate);
-		  
-		  String generalRecord = JOptionPane.showInputDialog("Enter General Record: ");
-		  student4.setGeneralRecord(generalRecord);
-		  
-		  String cpfNumber = JOptionPane.showInputDialog("Enter CPF number:");
-		  student4.setCpfNumber(cpfNumber);
-		  
-		  String matherName = JOptionPane.showInputDialog("Enter wiht mather name:");
-		  student4.setMatherName(matherName);
-		  
-		  String fatherName = JOptionPane.showInputDialog("Enter wiht father name:");
-		  student4.setFatherName(fatherName);
-		  
-		  String enrollmentDate =
-		  JOptionPane.showInputDialog("Enter with enrollment date:");
-		  student4.setEnrollmentDate(enrollmentDate);
-		  
-		  String schoolName = JOptionPane.showInputDialog("Enter School name:");
-		  student4.setSchoolName(schoolName);
-		  
-		  String seriesEnrollment = JOptionPane.showInputDialog("Enter series:");
-		  student4.setSeriesEnrollment(seriesEnrollment);
-		  
+		/*
+		 * int age = Integer.parseInt(JOptionPane.showInputDialog("Enter age:"));
+		 * student4.setAge(age);
+		 * 
+		 * 
+		 * String birthDate = JOptionPane.showInputDialog("Enter birth date:");
+		 * student4.setBirthDate(birthDate);
+		 * 
+		 * String generalRecord = JOptionPane.showInputDialog("Enter General Record: ");
+		 * student4.setGeneralRecord(generalRecord);
+		 * 
+		 * String cpfNumber = JOptionPane.showInputDialog("Enter CPF number:");
+		 * student4.setCpfNumber(cpfNumber);
+		 * 
+		 * String matherName = JOptionPane.showInputDialog("Enter wiht mather name:");
+		 * student4.setMatherName(matherName);
+		 * 
+		 * String fatherName = JOptionPane.showInputDialog("Enter wiht father name:");
+		 * student4.setFatherName(fatherName);
+		 * 
+		 * String enrollmentDate =
+		 * JOptionPane.showInputDialog("Enter with enrollment date:");
+		 * student4.setEnrollmentDate(enrollmentDate);
+		 * 
+		 * String schoolName = JOptionPane.showInputDialog("Enter School name:");
+		 * student4.setSchoolName(schoolName);
+		 * 
+		 * String seriesEnrollment = JOptionPane.showInputDialog("Enter series:");
+		 * student4.setSeriesEnrollment(seriesEnrollment);
+		 */
 		 
 		for (int i = 0; i <= 3; i++) {
 
@@ -61,9 +62,19 @@ public class FirstClass {
 		int choice = JOptionPane.showConfirmDialog(null, "Do you want delete any subject ?");
 
 		if (choice == 0) {
-
-			int optionUser = Integer.parseInt(JOptionPane.showInputDialog("Enter with number of subject: 1, 2, 3, 4"));
-			student4.getSubjects().remove(optionUser - 1);
+			
+			int removeContinue = 0;
+			
+			while(removeContinue == 0) {
+				
+				int position = 1;
+				
+				int optionUser = Integer.parseInt(JOptionPane.showInputDialog("Enter with number of subject: 1, 2, 3, 4"));
+				student4.getSubjects().remove(optionUser - position);
+				position ++;
+				
+				removeContinue = JOptionPane.showConfirmDialog(null, "Do you want delete other subject?");
+			}
 		}
 
 		System.out.println(student4.toString());
