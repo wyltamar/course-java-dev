3fe5aaed8ef80fb131a83c4168ce20a4e541ffc1
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-12 10:53:37 -0300

    Working with Arrays

---
+++ course-java-dev/src/executes/ArrayVector.java
@@ -0,0 +1,20 @@
+package executes;
+
+public class ArrayVector {
+
+	public static void main(String[] args) {
+
+		double [] grades = new double[4];
+		
+		grades[0] = 9.8;
+		grades[1] = 7.8;
+		grades[2] = 8.0;
+		grades[3] = 9.0;
+		
+		System.out.println("Note 1 = "+grades[0]);
+		System.out.println("Note 2 = "+grades[1]);
+		System.out.println("Note 3 = "+grades[2]);
+		System.out.println("Note 4 = "+grades[3]);
+	}
+
+}
