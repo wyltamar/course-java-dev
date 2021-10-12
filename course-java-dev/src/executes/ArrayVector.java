8668d2d97a225b4cc12e6c87bb2b8ab9a83b0bc0
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-12 11:48:53 -0300

    Dynamically input and read

--- course-java-dev/src/executes/ArrayVector.java
+++ course-java-dev/src/executes/ArrayVector.java
@@ -1,23 +1,23 @@
 package executes;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVector {
 
 	public static void main(String[] args) {
 
-		double [] grades = new double[4];
+		int notes = Integer.parseInt(JOptionPane.showInputDialog("How many positions should the Array have?")); 
 		
-		grades[0] = 9.8;
-		grades[1] = 7.8;
-		grades[2] = 8.0;
-		grades[3] = 9.0;
+		double [] grades = new double[notes];
+		
 		
 		for(int i = 0; i < grades.length; i++) {
 			
-			System.out.println("Note "+(i + 1)+" = "+grades[i]);
+			grades[i] = Double.parseDouble(JOptionPane.showInputDialog("Inform the grade "+(i+1)));
 		}
 		
-		for(double i : grades) {
-			System.out.println("grade  = "+i);
+		for(int i = 0; i < grades.length; i++) {
+			System.out.println("Grade "+(i + 1)+" = "+grades[i]);
 		}
 		
 	}
