88a73071c36e57d31cf38d0c58a0556cbae79f8b
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-12 16:27:55 -0300

    Discovering the highest grade of the student

--- course-java-dev/src/executes/ArrayVector.java
+++ course-java-dev/src/executes/ArrayVector.java
@@ -32,10 +32,28 @@ public class ArrayVector {
 		
 		for(Subject sub: student.getSubjects()) {
 			
+			double maxGrade = 0.0;
+			double minGrade = 100.0;
+			
 			System.out.println("Student's subject: "+sub.getSubject());
 			for(int i = 0 ; i < sub.getGrade().length; i++) {
 				System.out.println("Subject "+(i+1)+": "+sub.getGrade()[i]);
+				
+				if(i == 0) {
+					maxGrade = sub.getGrade()[i];
+					minGrade = sub.getGrade()[i];
+				}else {
+					if(sub.getGrade()[i] > maxGrade) {
+						maxGrade = sub.getGrade()[i];
+					}
+					if(sub.getGrade()[i] < minGrade) {
+						minGrade = sub.getGrade()[i];
+					}
+				}
+				
 			}
+				System.out.println("The largest grade of subject "+sub.getSubject()+" is: "+maxGrade);
+				System.out.println("The shortest grade of subject "+sub.getSubject()+" is: "+minGrade);
 		}
 		
 	}
