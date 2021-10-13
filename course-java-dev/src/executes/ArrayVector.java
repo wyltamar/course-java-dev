f8831d46d285bb8c9029dba6124650a7296f61c6
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-13 10:49:37 -0300

    Manipulating object arrays

--- course-java-dev/src/executes/ArrayVector.java
+++ course-java-dev/src/executes/ArrayVector.java
@@ -7,8 +7,10 @@ public class ArrayVector {
 
 	public static void main(String[] args) {
 		
-		double[] javaGrades = {9.0,10.0,7.8,8.4};
-		double[] logicaGrades = {80.0,90,7.8,8.4};
+		double[] javaGrades = {90,100,78,84};
+		double[] logicaGrades = {80,90,78,84};
+		double[] javaScriptGrades = {56,70,80,95};
+		double[] bdGrades = {100,80,70,65};
 
 		Student student = new Student();
 		student.setName("Wyltamar Dougals");
@@ -26,36 +28,41 @@ public class ArrayVector {
 		
 		student.getSubjects().add(subject2);
 		
-		System.out.println("------------Datas of student-----------");
-		System.out.println("Name: "+student.getName()+ "enrollment on "+student.getSchoolName());
-		System.out.println("=============Subjects names ===========");
+		Student student2 = new Student();
+		student2.setName("João Moura");
+		student2.setSchoolName("JDEV Treinamentos");
 		
-		for(Subject sub: student.getSubjects()) {
+		Subject subject3 = new Subject();
+		subject3.setSubject("JavaScript");
+		subject3.setGrade(javaScriptGrades);
+		
+		student2.getSubjects().add(subject3);
+		
+		Subject subject4 = new Subject();
+		subject4.setSubject("Banco de dados");
+		subject4.setGrade(bdGrades);
+		
+		//=======================================================================
+		
+		Student [] arrayStudents = new Student[2];
+		arrayStudents[0] = student;
+		arrayStudents[1] = student2;
+		
+		System.out.println("------------Datas Student-----------------");
+		for(int i = 0; i < arrayStudents.length; i++) {
+			System.out.println("Student name: "+arrayStudents[i].getName());
+			System.out.println("=======================================");
 			
-			double maxGrade = 0.0;
-			double minGrade = 100.0;
-			
-			System.out.println("Student's subject: "+sub.getSubject());
-			for(int i = 0 ; i < sub.getGrade().length; i++) {
-				System.out.println("Subject "+(i+1)+": "+sub.getGrade()[i]);
+			for(Subject sub : arrayStudents[i].getSubjects()) {
+				System.out.println("Subject: "+sub.getSubject());
 				
-				if(i == 0) {
-					maxGrade = sub.getGrade()[i];
-					minGrade = sub.getGrade()[i];
-				}else {
-					if(sub.getGrade()[i] > maxGrade) {
-						maxGrade = sub.getGrade()[i];
-					}
-					if(sub.getGrade()[i] < minGrade) {
-						minGrade = sub.getGrade()[i];
-					}
+				for(int j = 0; j < sub.getGrade().length; j++) {
+					
+					System.out.println("Grade "+(j+1)+": "+sub.getGrade()[j]);
 				}
-				
+				System.out.println("-------------------------------------");
 			}
-				System.out.println("The largest grade of subject "+sub.getSubject()+" is: "+maxGrade);
-				System.out.println("The shortest grade of subject "+sub.getSubject()+" is: "+minGrade);
 		}
-		
 	}
 
 }
