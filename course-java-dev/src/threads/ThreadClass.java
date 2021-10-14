b9ae8480021953421866953c8463304a3b0b7ba2
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-14 15:46:45 -0300

    Thread in Runnable object

--- course-java-dev/src/threads/ThreadClass.java
+++ course-java-dev/src/threads/ThreadClass.java
@@ -5,47 +5,50 @@ import javax.swing.JOptionPane;
 public class ThreadClass {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		new Thread() {
-			
-			public void run() {
-				
-				for(int i = 0; i < 10; i++) {
-					System.out.println("Sending email...");
-					
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				
-			};
-			
-		}.start();
-		
-		
-		new Thread() {
-			
-			public void run() {
-				
-				for(int i = 0; i < 10; i++) {
-					System.out.println("Sending invoice...");
-					
-					try {
-						Thread.sleep(3000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				
-			};
-			
-		}.start();
-		
-		
+
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
+
+		Thread threadInvoice = new Thread(thread2);
+		threadInvoice.start();
+
 		System.out.println("Finishing code exucute");
 		JOptionPane.showMessageDialog(null, "Finished program!");
 	}
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Sending email...");
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Sending invoice...");
+
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
 
 }
