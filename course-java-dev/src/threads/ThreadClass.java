fa4c27331f789966f37a6d890f708264f306dbe4
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-13 21:21:57 -0300

    Threads class 3

--- course-java-dev/src/threads/ThreadClass.java
+++ course-java-dev/src/threads/ThreadClass.java
@@ -1,16 +1,33 @@
 package threads;
 
+import javax.swing.JOptionPane;
+
 public class ThreadClass {
 
 	public static void main(String[] args) throws InterruptedException {
-
-		for(int i = 0; i < 10; i++) {
-			System.out.println("Sending email...");
+		
+		new Thread() {
 			
-			Thread.sleep(1000);
-		}
+			public void run() {
+				
+				for(int i = 0; i < 10; i++) {
+					System.out.println("Sending email...");
+					
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			};
+			
+		}.start();
+
+		
 		
 		System.out.println("Finishing code exucute");
+		JOptionPane.showMessageDialog(null, "Finished program!");
 	}
 
 }
