4011d6e48e8f1b3ce608e6181314094e3437f600
Wyltamar Douglas de Sousa Oliveira <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-14 11:12:50 -0300

    Porcessing competitors beetwen two threads

--- course-java-dev/src/threads/ThreadClass.java
+++ course-java-dev/src/threads/ThreadClass.java
@@ -14,7 +14,7 @@ public class ThreadClass {
 					System.out.println("Sending email...");
 					
 					try {
-						Thread.sleep(2000);
+						Thread.sleep(1000);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -23,6 +23,29 @@ public class ThreadClass {
 			};
 			
 		}.start();
+		
+		/*Competitors threads*/
+		public static void main(String[] args) throws InterruptedException {
+		
+		new Thread() {
+			
+			public void run() {
+				
+				for(int i = 0; i < 10; i++) {
+					System.out.println("Sending invoice...");
+					
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			};
+			
+		}.start();
+		
+		
 
 		
 		
