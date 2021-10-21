bbc3604bd96b0eb4982980ff61818a7d9b705241
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-21 15:19:34 -0300

    Knowing three different ways to format date and time

--- course-java-dev/src/dates/DatesInJava5.java
+++ course-java-dev/src/dates/DatesInJava5.java
@@ -13,11 +13,10 @@ public class DatesInJava5 {
 		/*New date API of Java 8 */
 		
 		LocalDate currentDate = LocalDate.now();
-		DateTimeFormatter currentDateFormated = DateTimeFormatter.ofPattern("dd-MM-yyy");
-		System.out.println("Current date formated: "+currentDate.format(currentDateFormated));
+		System.out.println("Current date formated: "+currentDate.format(DateTimeFormatter.ofPattern("dd/MM/yyy")));
 		
 		LocalTime currentHour = LocalTime.now();
-		System.out.println("Current hour formated: "+DateTimeFormatter.ofPattern("HH:mm.ss").format(currentHour));
+		System.out.println("Current hour formated: "+DateTimeFormatter.ofPattern("HH:mm:ss").format(currentHour));
 		
 		LocalDateTime currentDateTime = LocalDateTime.now();
 		DateTimeFormatter todayDateTime =  DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm.ss");
